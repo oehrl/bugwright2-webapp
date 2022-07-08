acde2567386c02935f70cf84144f602d1f60eaa5
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-07-08 09:48:57 +0200

    Start working on robot dialog

--- src/Robot.tsx
+++ src/Robot.tsx
@@ -1,8 +1,34 @@
+import { createContext, createSignal, PropsWithChildren } from "solid-js";
+
 export type RobotType = "Crawler";
 
-export default interface Robot {
+export interface Robot {
   connection: string;
   prefix?: string;
   robotType: RobotType;
   poseTopic: string;
+}
+
+export interface RobotList {
+  robots: Robot[];
+  add(robot: Robot): void;
+  remove(index: number): void;
+}
+
+const RobotListContext = createContext<RobotList>();
+
+export default function RobotListProvider(props: PropsWithChildren) {
+  const [robots, setRobots] = createSignal<Robot[]>([]);
+
+  return (
+    <RobotListContext.Provider
+      value={{
+        robots: robots(),
+        add: robot => setRobots([...robots(), robot]),
+        remove: index => setRobots(robots().filter((_, robotIndex) => index !== robotIndex)),
+      }}
+    >
+      {props.children}
+    </RobotListContext.Provider>
+  );
 }
